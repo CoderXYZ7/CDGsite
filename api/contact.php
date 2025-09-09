7f2a213e6f5cd0c2150e67c91d28fc8394c0824d
coderXYZ <coderXYZ7@outlook.com>
2025-09-09 10:50:54 +0200

    Revert "yrdying"
    This reverts commit aeb5315ee5c7dab5de822253652936112750dce8.

--- api/contact.php
+++ api/contact.php
@@ -1,8 +1,4 @@
 <?php
-// Enable error reporting for debugging
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 // Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
